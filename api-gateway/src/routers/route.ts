c1baae7cd598819290f34592f7ca84aa1ac64936
julyca <jufsantana1@gmail.com>
2022-06-19 20:47:47 -0300

    feat: new /api/users endpoint; update imports references

--- api-gateway/src/routers/route.ts
+++ api-gateway/src/routers/route.ts
@@ -1,13 +1,13 @@
 import express from 'express'
-import login from '../handlers/login'
-import refresh from '../handlers/refresh'
-import register from '../handlers/register'
+import { login, refresh } from '../handlers/auth.handler'
+import { listUsers, register } from '../handlers/user.handler'
 
 const router = express.Router()
 
 router.post('/register', register)
-
 router.post('/auth', login)
+
 router.get('/refresh', refresh)
+router.get('/users', listUsers)
 
 export default router
