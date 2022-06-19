9c37e431caac9d05d62369be743475a7a3b347b8
julyca <jufsantana1@gmail.com>
2022-06-19 19:19:22 -0300

    fix: changing mongo-init users passwords to sha256

--- api-gateway/database/mongo-init.js
+++ api-gateway/database/mongo-init.js
@@ -12,21 +12,21 @@ db.createUser({
 db.user.insertMany([
  {
    username: 'potato',
-   password: 'batata',
+   password: 'f4610aa514477222afac2b77f971d069780ca2846f375849f3dfa3c0047ebbd1', // batata
    fullName: 'Potato Potato',
    isActive: true,
    isDeleted: false
   },
   {
     username: 'tomato',
-    password: 'tomate',
+    password: 'cea70552116071b4ea769c41150f7f76c0e7673bfd6dc62453d507e41af9b529', // tomate
     fullName: 'Tomato Tomate',
     isActive: true,
     isDeleted: false
    },
    {
     username: 'bruh',
-    password: 'bruh',
+    password: '408f31d86c6bf4a8aff4ea682ad002278f8cb39dc5f37b53d343e63a61f3cc4f', // bruh
     fullName: 'Bruh d Bro',
     isActive: true,
     isDeleted: false
