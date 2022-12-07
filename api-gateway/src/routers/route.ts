68463fd774bacb36a20a633b66fd4a8c8c829e45
julyca <jufsantana1@gmail.com>
2022-12-06 23:24:00 -0300

    feat: add cards microsservice integration

--- api-gateway/src/routers/route.ts
+++ api-gateway/src/routers/route.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { login, refresh } from '../handlers/auth.handler'
+import { findCardsByName, getCard, listCards } from '../handlers/card.handler'
 import { listUsers, register } from '../handlers/user.handler'
 
 const router = express.Router()
@@ -9,5 +10,9 @@ router.post('/auth', login)
 
 router.get('/refresh', refresh)
 router.get('/users', listUsers)
+
+router.get('/cards', listCards)
+router.get('/cards/:id', getCard)
+router.get('/cards/:name', findCardsByName)
 
 export default router
